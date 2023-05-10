4bd827e7a180cffec9faedc33e46019404963fd5
andy jan <andy_jan@hotmail.com>
2023-05-09 22:44:25 -0500

    v0 deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,46 @@
-
 import axios from "axios";
+import { Suspense } from "react";//!only used on component
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const Page=async( )=>{
-await sleep(5000)
- const res=await axios.get('https://jsonplaceholder.typicode.com/users/1').then(res=>res.data)
-
- 
-//throw new Error('not found')
+const Page = async () => {
+//  await sleep(5000);
 
 
+console.log(JSON.parse(dd));
+console.log(dd.json);
+  const res = await axios
+    .get("https://jsonplaceholder.typicode.com/users/1",{next:{revalidate:60}})
+    .then((res) => res.data);
 
-   
-    console.log(res)
+
+
+
+  return (
   
-    return <>{JSON.stringify(res)}</>
+   <>{JSON.stringify(res)}</>   
+ 
+  );
+};
+
+export default function MyPage() {
+  return ( <Suspense fallback={<div>Suspense..</div>}>  <Page />;</Suspense>
+)
 }
 
-export default Page;
+
+let dd=JSON.stringify ({
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+    "lat": "-37.3159",
+    "lng": "81.1496"
+    }}})
+    
+   
